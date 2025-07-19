1ff19986967c219451f6aa9f0c52955fb2134a50
Wikan Setiaji <wikansetiaji@gmail.com>
2025-07-19 20:33:51 +0700

    Fix: widows fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,33 @@ import 'package:kiosk_painting_info/pages/split_page.dart';
 import 'package:kiosk_painting_info/services/language_provider.dart';
 import 'package:kiosk_painting_info/services/size_config.dart';
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:window_size/window_size.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (Platform.isWindows || Platform.isMacOS) {
     setWindowTitle('Your App');
     setWindowMinSize(const Size(960, 540));
     setWindowMaxSize(Size.infinite);
-    setWindowFrame(const Rect.fromLTWH(100, 100, 1280, 720)); // 16:9
   }
 
   runApp(
     ChangeNotifierProvider(create: (_) => LanguageProvider(), child: MyApp()),
   );
+
+  if (Platform.isWindows) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await windowManager.ensureInitialized();
+
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setFullScreen(true);
+      await windowManager.center();
+      await windowManager.show();
+      await windowManager.setSkipTaskbar(false);
+    });
+  }
 }
 
 class MyApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import screen_retriever_macos
+import window_manager
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
